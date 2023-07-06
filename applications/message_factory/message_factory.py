e8a938ef4af98764402596264d45b574471e7696
Kirill Plakhau <kirill.plakhau@outstaff.exness.com>
2023-07-06 17:47:10 +0300

    commented some code

--- applications/message_factory/message_factory.py
+++ applications/message_factory/message_factory.py
@@ -7,31 +7,31 @@ from uuid import UUID
 from pydantic import BaseModel, Field, NonNegativeInt, constr, validator
 
 
-class EventType(str, Enum):
-    form_send = 'form_send'
-    caf_form = 'caf_form'
-    caf_nav = 'caf_nav'
-    scroll = 'scroll'
-    page_open = 'page_open'
-    page_close = 'page_close'
-    pa_enter = 'pa_enter'
-    valid_event_types: set[str] = {et.value for et in EventType}
+# class EventType(str, Enum):
+#     form_send = 'form_send'
+#     caf_form = 'caf_form'
+#     caf_nav = 'caf_nav'
+#     scroll = 'scroll'
+#     page_open = 'page_open'
+#     page_close = 'page_close'
+#     pa_enter = 'pa_enter'
+#     valid_event_types: set[str] = {et.value for et in EventType}
 
 
-class ValidationMixin:
-    @validator('*', pre=True)
-    def convert_to_none(cls, v):
-        """Convert string value "None" into python None."""
-        return None if v == 'None' else v
-    @validator('event_type', pre=True)
-    def validate_event_type(cls, v):
-        """Validate event type."""
-        if v in valid_event_types:
-            raise ValueError(f'Wrong event_type: {v}')
-        return v
+# class ValidationMixin:
+#     @validator('*', pre=True)
+#     def convert_to_none(cls, v):
+#         """Convert string value "None" into python None."""
+#         return None if v == 'None' else v
+#     @validator('event_type', pre=True)
+#     def validate_event_type(cls, v):
+#         """Validate event type."""
+#         if v in valid_event_types:
+#             raise ValueError(f'Wrong event_type: {v}')
+#         return v
 
 
-class IAFSourceMessage(ValidationMixin, BaseModel):
+class IAFSourceMessage(BaseModel): # ValidationMixin,
     track_uid: Optional[UUID]
     user_uid: Optional[UUID]
     session_uid: Optional[UUID] = Field(default=None, alias='session_id')
